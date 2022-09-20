c73bdd756437c03480cf496aa004c5f60307baff
BT_4N2_02 <test@email.com>
2022-09-20 11:47:11 -0400

    Added a print statement about movies

--- src/Sample.java
+++ src/Sample.java
@@ -1,5 +1,6 @@
 public class Sample {
     public static void main (String[] args) {
         System.out.println("Hello World");
+        System.out.println("My favorite movie is Spider-Man: No Way Home.");
     }
 }
