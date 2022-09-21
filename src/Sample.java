4e6fef7e785838ec57d4ec25b1f53f0e7ebe9077
Knyc8 <kchen9712@bths.edu>
2022-09-21 18:20:43 -0400

    Added a print statement for my favorite food

--- src/Sample.java
+++ src/Sample.java
@@ -2,5 +2,6 @@ public class Sample {
     public static void main (String[] args) {
         System.out.println("Kenny Chen");
         System.out.println("My favorite movie is Spider-Man: No Way Home.");
+        System.out.println("My favorite food is sushi.");
     }
 }
